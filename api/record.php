4f0557349a8b9307bae1d7a84e93a0222d31933d
WeRockStar <vr_freestyle@outlook.com>
2016-06-12 02:38:12 +0700

    Uncomment

--- api/record.php
+++ api/record.php
@@ -8,8 +8,7 @@
   $item['range'] = array();
 
   $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db, $us, $pw, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-  //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $conn->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf8'");
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $result = $conn->query('SELECT  *FROM age');
   while ($row = $result->fetch()){
       $res['code'] = $row['age_code'];
