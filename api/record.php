c673754ee0b7c182c8d110338f0726221c261b37
WeRockStar <vr_freestyle@outlook.com>
2016-06-12 18:34:38 +0700

    Added GEO_ID

--- api/record.php
+++ api/record.php
@@ -23,5 +23,35 @@
       array_push($item['job'], $res);
   }
 
+  $res = array();
+  $item['department'] = array();
+  $result = $conn->query('SELECT  *FROM geography');
+  while ($row = $result->fetch()){
+      $res['dept_id'] = $row['GEO_ID'];
+      $res['dept_name'] = $row['GEO_NAME'];
+      array_push($item['department'], $res);
+  }
+
+  $res = array();
+  if($_GET['GEO_ID']) {
+    $item['province'] = array();
+    $result = $conn->query('SELECT  *FROM province WHERE GEO_ID = '.$_GET['GEO_ID']);
+    while ($row = $result->fetch()){
+        $res['province_id'] = $row['prov_code'];
+        $res['province_name'] = $row['prov_name'];
+        array_push($item['province'], $res);
+    }
+  }
+  $res = array();
+  if($_GET['prov_code'] != "") {
+    $item['district'] = array();
+    $result = $conn->query('SELECT district.prov_code,province.prov_code FROM district INNER JOIN province ON district.prov_code = '.$_GET['prov_code']);
+    while ($row = $result->fetch()){
+        $res['province_id'] = $row['prov_code'];
+        $res['district_id'] = $row['dis_code'];
+        $res['district_name'] = $row['DISTRICT_NAME'];
+        array_push($item['district'], $res);
+    }
+  }
   echo json_encode($item);
  ?>
